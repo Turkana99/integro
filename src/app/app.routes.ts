b02e76c66c75abd9d87f630853f4b649302ff77e
Turkana Mammadova <74309514+Turkana99@users.noreply.github.com>
2024-08-09 20:06:58 +0400

    Add first changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/main',
+  },
+  {
+    path: 'main',
+    component: MainComponent,
+  },
+];
