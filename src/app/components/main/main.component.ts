a043d73628d946809714ece980989f2ae8518588
Turkana Mammadova <74309514+Turkana99@users.noreply.github.com>
2024-08-13 18:05:57 +0400

    add  new changes

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DividerModule } from 'primeng/divider';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [DividerModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  currentYear: number | undefined;
+  ngOnInit(): void {
+    this.currentYear = new Date().getFullYear();
+  }
 
 }
